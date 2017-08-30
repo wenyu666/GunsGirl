525c212ad5f53ff14a3664639dfecb2e2ec81e39
wenyu666 <a2063016620@outlook.com>
2017-08-30 19:45:05 +0800

    TransformHelper
    填充TransformHelper脚本

--- GunsGirl/Assets/Scripts/Tools/TransformHelper.cs
+++ GunsGirl/Assets/Scripts/Tools/TransformHelper.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
 /// 查找工具
 /// </summary>
-public class TransformHelper : MonoBehaviour {
+public class TransformHelper : MonoBehaviour
+{
 
 
     /// <summary>
@@ -13,27 +15,43 @@ public class TransformHelper : MonoBehaviour {
     /// </summary>
     /// <param name="root">根节点</param>
     /// <param name="targetName">想要寻找的子物体的名字</param>
-    public static Transform FindChild(Transform root,string targetName)
+    public static Transform FindChild(Transform root, string name)
     {
-        //Transform currentNode = root;//当前节点
+        Transform target = root.Find(name);
+        if (target!=null){return target;}
 
-        if (root.transform.childCount<=0)
-        {
-            Debug.LogError("没有找到目标，请检查输入");
-            return default(Transform);
-        }
-        if (root.name==targetName)
-        {
-            Debug.Log("查找成功");
-            return root;
-        }
-
-        Transform tra = null;
+        Transform tra=null;
         for (int i = 0; i < root.childCount; i++)
         {
-            
+            tra = FindChild(root.GetChild(i), name);
+
+            //找到目标，阻断递归
+            if (tra!=null){return tra;}
         }
-        //currentNode = FindChild(currentNode, currentNode.name);
-        return default(Transform);
+
+        return null;
+    }
+
+    /// <summary>
+    /// 查找根节点下的组件
+    /// </summary>
+    /// <typeparam name="T">目标组件</typeparam>
+    /// <param name="root">根节点</param>
+    /// <returns></returns>
+    public static T FindChild<T>(Transform root) where T:MonoBehaviour
+    {
+        T target = root.GetComponentInChildren<T>();
+        if (target != null) { return target; }
+
+        T tra = default(T);
+        for (int i = 0; i < root.childCount; i++)
+        {
+            tra = FindChild<T>(root.GetChild(i));
+
+            //找到目标，阻断递归
+            if (tra != null) { return tra; }
+        }
+
+        return default(T);
     }
 }
